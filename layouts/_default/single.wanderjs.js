30cf4301450316eadec9b8bf2afdbd59e7b7cd27
Dilip <igoldlip@gmail.com>
2026-05-01 12:18:29 +0530

    show full urls for console + add desc text

--- layouts/_default/single.wanderjs.js
+++ layouts/_default/single.wanderjs.js
@@ -1,8 +1,8 @@
 {{- $page := site.GetPage "/wander" -}}
 {{- $dts := findRE `<dt>(?s:.*?)</dt>` $page.Content -}}
-{{- $consoles := split ($page.Params.wanderconsoles | default "") " " -}}
+{{- $consoles := split ($page.Params.wanderconsoles | default "") "|" -}}
 const wander = {
-  consoles: [{{ range $consoles }}{{ if . }}{{ . | jsonify }},{{ end }}{{ end }}],
+  consoles: [{{ range $consoles }}{{ if . }}{{ (index (split . "::") 0) | jsonify }},{{ end }}{{ end }}],
   pages: [{{ range $i, $dt := $dts }}{{ $url := $dt | replaceRE `(?s).*<a href="?([^ ">]+)"?.*` "$1" }}{{ $url | jsonify }},{{ end }}],
   styles: ["wander-theme.css"],
   ignore: [],
